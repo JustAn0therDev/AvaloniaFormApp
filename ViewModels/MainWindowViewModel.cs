2ec006b950c6a1966fbf125c4eeb659a3b74807f
Ruan <ruanmontelo@gmail.com>
2025-02-11 08:38:30 -0300

    Added a progress bar and a task to make the progress bar fill up.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -7,31 +7,42 @@ namespace AvaloniaFormApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
 
-    [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+    [ObservableProperty] [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
     private string _username = string.Empty;
-    
-    [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+
+    [ObservableProperty] [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
     private string _password = string.Empty;
-    
-    [ObservableProperty]
-    private bool _isLoadingVisible;
+
+    [ObservableProperty] private bool _isLoadingVisible;
+
+    [ObservableProperty] private int _loadingProgress;
 
     [RelayCommand(CanExecute = nameof(CanLogin))]
     private async Task Login()
     {
-        IsLoadingVisible = true;
+        Task loadingProgress = ProgressUp();
         
+        IsLoadingVisible = true;
+
         await Task.Delay(TimeSpan.FromSeconds(2));
+
+        await loadingProgress;
+
+        IsLoadingVisible = false;
         
         Greeting = $"Welcome, {Username}!";
-        
-        IsLoadingVisible = false;
     }
-    
+
+    private async Task ProgressUp()
+    {
+        while (LoadingProgress < 100)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(20));
+            LoadingProgress++;
+        }
+    }
+
     private bool CanLogin() => !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
 }
