2165a5e8f026cf9c5aa591c4d94c5402d9db1172
Ruan <ruanmontelo@gmail.com>
2025-02-11 00:28:43 -0300

    Initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AvaloniaFormApp.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _greeting = "Welcome to Avalonia!";
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+    private string _username = string.Empty;
+    
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+    private string _password = string.Empty;
+
+    [RelayCommand(CanExecute = nameof(CanLogin))]
+    private async Task Login()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(2));
+        
+        Greeting = $"Welcome, {Username}!";
+    }
+    
+    private bool CanLogin() => !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
+}
